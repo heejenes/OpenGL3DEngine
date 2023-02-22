c15885bda7fb84971299dd7fbc19a0c0f8263fa5
heejenes <turtlescubing@gmail.com>
2023-02-22 04:24:17 -0500

    Implemented simple shaders and the ability to load verticies

--- OpenGL3DEngine/OpenGLLoader.h
+++ OpenGL3DEngine/OpenGLLoader.h
@@ -1,13 +1,91 @@
 #pragma once
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <string>
 #include <vector>
 #include "Vertex.h"
+
+static void LoadVertexShader(const char* vertexShaderSource, unsigned int& vertexShader) {
+	
+	// Create shader of type GL_VERTEX_SHADER and store id in vertexShader
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	// link shader source code to shader in memory
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	// compile shader
+	glCompileShader(vertexShader);
+	// look for errors
+	int  success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		writeLog("ERROR::SHADER::VERTEX::COMPILATION_FAILED\n");
+		writeLog(infoLog);
+	}
+}
+
+static void LoadFragShader(const char* fragmentShaderSource, unsigned int& fragmentShader) {
+
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+	// look for errors
+	int  success;
+	char infoLog[512];
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		writeLog("ERROR::SHADER::FRAG::COMPILATION_FAILED\n");
+		writeLog(infoLog);
+	}
+}
+
+static void LoadShaders(unsigned int& shaderProgram, unsigned int& vertexShader, unsigned int& fragmentShader) {
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	// look for errors
+	int  success;
+	char infoLog[512];
+	glGetShaderiv(shaderProgram, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(shaderProgram, 512, NULL, infoLog);
+		writeLog("ERROR::SHADER::ATTACHING::COMPILATION_FAILED\n");
+		writeLog(infoLog);
+	}
+	// Makes any shaders/rendering after this point be done with our shaders
+	glUseProgram(shaderProgram);
+	// clears objects
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+}
+
+static void LoadVertices(const std::vector<Vertex>& vertexData) {
+
+	// VBO (vertex buffer object) is of type GL_ARRAY_BUFFER
+	uint32_t VBO;
+	// creates a buffer objects in memory and assigns VBO to a buffer ID
+	// that can be used to access that object. The first parameter is for
+	// number of buffer items to create
+	glGenBuffers(1, &VBO);
+	// We can "bind" to several different buffers but only one of each type
+	// bind generated buffer to GL_ARRAY_BUFFER
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	// copy the vertexData to the buffer.
+	// 4th parameter takes how GPU should manage data
+	// OPTIONS:
+	// GL_STREAM_DRAW: the data is set only once and used by the GPU at most a few times.
+	// GL_STATIC_DRAW: the data is set only once and used many times.
+	// GL_DYNAMIC_DRAW: the data is changed a lot and used many times.
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertexData), vertexData.data(), GL_STATIC_DRAW);
+}
 
 static DrawDetails UploadMesh(const std::vector<Vertex>& vertices, const std::vector<uint32_t> element) {
 	if (vertices.empty() || element.empty()) {
 		throw ("empty vector");
 	}
+	// VBO: Vertex buffer objects: where the verticies are stored on the GPU
 	uint32_t VAO, VBO, EBO;
 
 	glGenVertexArrays(1, &VAO);
--- OpenGL3DEngine/Source.cpp
+++ OpenGL3DEngine/Source.cpp
@@ -3,6 +3,7 @@
 #include "Callbacks.h"
 #include "UtilityFunctions.h"
 #include "Vertex.h"
+#include "OpenGLLoader.h"
 #include <vector>
 
 int main(int argc, char** argv) {
@@ -29,23 +30,59 @@ int main(int argc, char** argv) {
 	glfwSetWindowCloseCallback(window, glfwWindowCloseCallback);
 
 	glClearColor(.2f, .2f, .6f, 0.f);
-	std::vector<DrawDetails> drawDetails;
-	{
-		std::vector<Vertex> objPts;
-		// each line is a "point", the three lines make a triangle
-		objPts.emplace_back(.5f, -.5f, 0.f);
-		objPts.emplace_back(-.5f, -.5f, 0.f);
-		objPts.emplace_back(0.f, .5f, 0.f);
 
-		// element array buffer
-		std::vector<uint32_t> elem = { 0, 1, 2 };
+	// Coordinates must be between -1 and 1 to appear on the screen. 
+	// This is called "normalized device coordinates (NDC)"
+	std::vector<Vertex> vertexData;
+	// each line of code is a 3d point, the three lines make a triangle
+	vertexData.emplace_back(.5f, -.5f, 0.f);
+	vertexData.emplace_back(-.5f, -.5f, 0.f);
+	vertexData.emplace_back(0.f, .5f, 0.f);
 
-		drawDetails.push_back(UploadMesh(objPts, elem));
-	}
+	LoadVertices(vertexData);
+
+	unsigned int vertexShader;
+	const char* vertexShaderSource =
+		"#version 330 core\n"
+		"layout (location = 0) in vec3 aPos;\n"
+		"void main()\n"
+		"{\n"
+		"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+		"}\0";
+	LoadVertexShader(vertexShaderSource, vertexShader);
+
+	unsigned int fragmentShader;
+	const char* fragShaderSource =
+		"#version 330 core"
+		"out vec4 FragColor;"
+		"void main()"
+		"{"
+		"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);"
+		"}";
+	LoadFragShader(fragShaderSource, fragmentShader);
+
+	unsigned int shaderProgram;
+	LoadShaders(shaderProgram, vertexShader, fragmentShader);
+
+	// Tell OpenGL how we've organized our vertices data (Looks at vertices data from the GL_ARRAY_BUFFER VBO)
+	// Arg 1: which vertex attribute to configure ("location = 0" from vertexShaderSource)
+	// Arg 2: size of vertex. 3 for x, y, z
+	// Arg 3: Type of data (x, y, z are floats)
+	// Arg 4: if we want data to be "normalized" (different from NDC, just ignore for now)
+	// Arg 5: stride (number of bits between vertices = 3*(size of float))
+	// Arg 6: "offset of where the position data begins in buffer" learn more later.
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
 	while (!glfwWindowShouldClose(window)) {
+		// input handling
 		processInput(window);
+
+		// rendering
+	
+		// clears specified buffer with color specified in glClearColor(). 
+		// (options: GL_COLOR_BUFFER_BIT, GL_DEPTH_BUFFER_BIT, GL_STENCIL_BUFFER_BIT)
 		glClear(GL_COLOR_BUFFER_BIT);
-		Draw(drawDetails);
 
 		// Swaps the front and back buffers. front buffer is the buffer 
 		// that is displayed, back buffer is the new frame being drawn 
