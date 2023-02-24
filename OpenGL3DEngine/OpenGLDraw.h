b079843ce544d318f78cf64495afe9130266e0db
Taehoon K <turtlescubing@gmail.com>
2023-02-24 04:48:50 -0500

    Can load textures onto meshes

--- OpenGL3DEngine/OpenGLDraw.h
+++ OpenGL3DEngine/OpenGLDraw.h
@@ -5,11 +5,12 @@
 
 #include "Shader.h"
 
-static void DrawVertices(Shader shader, unsigned int VAO, int indicesSize) {
+static void DrawVertices(Shader shader, unsigned int texture, unsigned int VAO, int indicesSize) {
 	shader.use();
 	glBindVertexArray(VAO);
 	glDrawElements(GL_TRIANGLES, indicesSize, GL_UNSIGNED_INT, 0);
 	glBindVertexArray(0);
 	glBindVertexArray(1);
+	glBindTexture(GL_TEXTURE_2D, texture);
 }
 #endif
